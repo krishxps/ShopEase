b61396482ccd6311a64eaacde8f9f905e245534a
Krish Mac <99328839+krishxps@users.noreply.github.com>
2024-06-24 19:17:49 -0400

    Add Item Added

--- server.js
+++ server.js
@@ -13,10 +13,22 @@ GitHub Repository URL: https://github.com/krishxps/web322-app
 
 const express = require('express');
 const path = require('path');
+const multer = require('multer');
+const cloudinary = require('cloudinary').v2;
+const streamifier = require('streamifier');
+
 const storeService = require('./store-service');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+cloudinary.config({
+    cloud_name: 'dh0zkzgpk',
+    api_key: '645712828111368',
+    api_secret: 'FJuB75gHkggMxdNmaG8EPSehV5w'
+});
+
+const upload = multer();
 
 app.use(express.static('public'));
 
@@ -45,6 +57,50 @@ app.get('/categories', (req, res) => {
         .then(data => res.json(data))
         .catch(err => res.status(500).json({ message: err }));
 });
+
+app.get('/items/add', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'additem.html'));
+})
+
+app.post('/items/add', upload.single('featureImage'), (req, res,next) => {
+    if(req.file){
+        let streamUpload = (req) => {
+            return new Promise((resolve, reject) => {
+                let stream = cloudinary.uploader.upload_stream(
+                    (error, result) => {
+                        if (result) {
+                            resolve(result);
+                        } else {
+                            reject(error);
+                        }
+                    }
+                );
+    
+                streamifier.createReadStream(req.file.buffer).pipe(stream);
+            });
+        };
+    
+        async function upload(req) {
+            let result = await streamUpload(req);
+            console.log(result);
+            return result;
+        }
+    
+        upload(req).then((uploaded)=>{
+            processItem(uploaded.url);
+        });
+    }else{
+        processItem("");
+    }
+     
+    function processItem(imageUrl){
+        req.body.featureImage = imageUrl;
+        // TODO: Process the req.body and add it as a new Item before redirecting to /items
+        storeService.addItem(req.body)
+            .then(() => res.redirect('/items'))
+            .catch(err => res.status(500).json({ message: err }));
+    }
+})
 
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
--- store-service.js
+++ store-service.js
@@ -1,71 +1,103 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
+const { features, title } = require("process");
 
 let items = [];
 let categories = [];
 
 const initialize = () => {
-    return new Promise((resolve, reject) => {
-        fs.readFile(path.join(__dirname, 'data', 'items.json'), 'utf8', (err, data) => {
-            if (err) {
-                reject('unable to read items file');
-                return;
-            }
-            try {
-                items = JSON.parse(data);
-            } catch (err) {
-                reject('unable to parse items file');
-            }
-        });
-        fs.readFile(path.join(__dirname, 'data', 'categories.json'), 'utf8', (err, data) => {
-            if (err) {
-                reject('unable to read categories file');
-                return;
-            }
-            try {
-                categories = JSON.parse(data);
-                resolve();
-            } catch (err) {
-                reject('unable to parse categories file');
-            }
-        });
-    });
+  return new Promise((resolve, reject) => {
+    fs.readFile(
+      path.join(__dirname, "data", "items.json"),
+      "utf8",
+      (err, data) => {
+        if (err) {
+          reject("unable to read items file");
+          return;
+        }
+        try {
+          items = JSON.parse(data);
+        } catch (err) {
+          reject("unable to parse items file");
+        }
+      }
+    );
+    fs.readFile(
+      path.join(__dirname, "data", "categories.json"),
+      "utf8",
+      (err, data) => {
+        if (err) {
+          reject("unable to read categories file");
+          return;
+        }
+        try {
+          categories = JSON.parse(data);
+          resolve();
+        } catch (err) {
+          reject("unable to parse categories file");
+        }
+      }
+    );
+  });
 };
 
 const getAllItems = () => {
-    return new Promise((resolve, reject) => {
-        if (items.length > 0) {
-            resolve(items);
-        } else {
-            reject('no results returned');
-        }
-    });
+  return new Promise((resolve, reject) => {
+    if (items.length > 0) {
+      resolve(items);
+    } else {
+      reject("no results returned");
+    }
+  });
 };
 
 const getPublishedItems = () => {
-    return new Promise((resolve, reject) => {
-        const publishedItems = items.filter(item => item.published);
-        if (publishedItems.length > 0) {
-            resolve(publishedItems);
-        } else {
-            reject('no results returned');
-        }
-    });
+  return new Promise((resolve, reject) => {
+    const publishedItems = items.filter((item) => item.published);
+    if (publishedItems.length > 0) {
+      resolve(publishedItems);
+    } else {
+      reject("no results returned");
+    }
+  });
 };
 
 const getCategories = () => {
-    return new Promise((resolve, reject) => {
-        if (categories.length > 0) {
-            resolve(categories);
-        } else {
-            reject('no results returned');
-        }
-    });
+  return new Promise((resolve, reject) => {
+    if (categories.length > 0) {
+      resolve(categories);
+    } else {
+      reject("no results returned");
+    }
+  });
+};
+
+const addItem = (item) => {
+  return new Promise((resolve, reject) => {
+    if (item.published === undefined) {
+      item.published = false;
+    } else {
+      item.published = true;
+    }
+    const newItem = {
+      id: items.length + 1,
+      categories: item.categories,
+      postDate: new Date().toISOString().split("T")[0],
+      featuresImage: item.featureImage,
+      price: parseFloat(item.price).toFixed(2) || item.price,
+      title: item.title,
+      body: item.body,
+      published: item.published,
+    };
+    items.push(newItem);
+    resolve(newItem);
+  });
 };
 
 module.exports = {
-    initialize,
-    getAllItems,
-    getPublishedItems,
-    getCategories
+  initialize,
+  getAllItems,
+  getPublishedItems,
+  getCategories,
+  addItem,
 };
