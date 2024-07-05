f64e580c788b7984aa903f331353947782a99aac
Krish Mac <99328839+krishxps@users.noreply.github.com>
2024-07-05 13:36:53 -0400

    Trying to fix internal server error vercel

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ const upload = multer();
 /// Custom Middleware
 //---------------------------------------------------------------------------
 app.set("view engine", "hbs");
-
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static("public"));
 
 app.use(function (req, res, next) {
